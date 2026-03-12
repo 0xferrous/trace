7022fd1b5efb6948c1955759d27e57cf17ebeebe
0xferrous <213212767+0xferrous@users.noreply.github.com>
2026-03-12 04:04:19 +0000

    chore: update SVM releases list hash

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
 
         svmReleasesList = pkgs.fetchurl {
           url = "https://binaries.soliditylang.org/linux-amd64/list.json";
-          sha256 = "sha256-PW2PIIS+2AMIVzzoHJgnHd7OpydhXvkFkEk7dDJUqpQ=";
+          sha256 = "sha256-L3zgoNUWLfEAFMNtPGTnIkn9+fCwRYNFlv7u8bysS9E=";
         };
 
         commonArgs = {
